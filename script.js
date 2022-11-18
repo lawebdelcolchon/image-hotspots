9d330e6cae42e9cd09cc0dfed3aedfd45cef5343
lawebdelcolchon <105754172+lawebdelcolchon@users.noreply.github.com>
2022-11-18 14:05:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// const hotspots = document.querySelectorAll('.image-hotspots__hotspot');
+
+// hotspots.forEach(hotspot => {
+//   hotspot.addEventListener('mouseover', addLabel);
+//   hotspot.addEventListener('mouseout', removeLabel);
+// });
+
+// function addLabel(e){
+//   const selectedHotspot = e.target.parentElement;
+//   selectedHotspot.classList.add('image-hotspots--selected');
+// }
+
+// function removeLabel(e){
+//   const selectedHotspot = e.target.parentElement;
+//   selectedHotspot.classList.remove('image-hotspots--selected');
+// }
